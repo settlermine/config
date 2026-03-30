d41f3b1f4fc5c7d325f1a4bf956b07d84a9ddc72
Artem Andrenkov <settlermine@yandex.ru>
2026-03-30 11:02:17 +0700

    Updated theme:

--- dotfiles/.config/nvim/lua/plugins/jetbrains.lua
+++ dotfiles/.config/nvim/lua/plugins/jetbrains.lua
@@ -5,6 +5,9 @@ return {
     priority = 1000,
     config = function()
       vim.cmd([[colorscheme darcula]])
+      
+      -- Фикс темных системных файлов
+      vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { fg = "#999999" })
     end,
   },
 }
