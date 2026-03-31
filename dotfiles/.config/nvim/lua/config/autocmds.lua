9cd0f137fdbffc7069903d6a9f258a58a20125c5
Artem Andrenkov <settlermine@yandex.ru>
2026-03-31 21:27:17 +0700

    updated config

--- dotfiles/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Чтобы LazyVim искал файлы в той репе, в которой открыт текущий файл
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local file = vim.api.nvim_buf_get_name(0)
+    if file == "" then
+      return
+    end
+
+    local dir = vim.fn.fnamemodify(file, ":p:h")
+    local git_root = vim.fn.systemlist("git -C " .. dir .. " rev-parse --show-toplevel")[1]
+
+    if git_root and git_root ~= "" then
+      vim.cmd("lcd " .. git_root)
+    end
+  end,
+})
