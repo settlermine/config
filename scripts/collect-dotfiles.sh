8e49bf8d60101272469e0b30530cc419b102883d
Artem Andrenkov <settlermine@yandex.ru>
2026-06-06 14:40:54 +0700

    upd

--- scripts/collect-dotfiles.sh
+++ scripts/collect-dotfiles.sh
@@ -5,6 +5,7 @@ echo "===> Collecting dotfiles..."
 cp ~/.zshrc ../dotfiles
 cp ~/.tmux.conf ../dotfiles
 cp -r ~/.config/nvim/ ../dotfiles/.config
+cp -r ~/.config/alacritty/ ../dotfiles/.config
 
 echo "===> Done!"
 
