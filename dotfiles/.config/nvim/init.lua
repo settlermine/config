b0f13234d0382b59481180bd8fe4b5ec4b66bc6c
settlermine <settlermine@yandex.ru>
2026-08-02 12:38:01 +0700

    upd

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,5 +1,29 @@
 -- Need to install sudo pacman -S tree-sitter-cli base-devel curl zathura zathura-pdf-mupdf texlive-core luakit node npm 
 -- Also needed to execute :call mkdp#util#install() in comand line once
+
+--------------------------------------------------
+-- UTILS 
+--------------------------------------------------
+-- TODO: fix for oil
+local function get_current_path()
+	if vim.bo.filetype == "oil" then
+		return require("oil").get_current_dir()
+	end
+	return vim.api.nvim_buf_get_name(0)
+end
+
+local function get_git_root()
+	local path = get_current_path()
+    if path == nil then
+        return
+    else
+	    return vim.fs.root(path, ".git")
+    end
+end
+
+--------------------------------------------------
+-- VIM 
+--------------------------------------------------
 vim.g.mapleader = ' '
 vim.g.vimtex_view_method = "zathura"
 vim.g.mkdp_browser = "luakit"
@@ -8,14 +32,17 @@ vim.g.mkdp_theme = "dark"
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.signcolumn = "yes"
+-- required for auto-session
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.swapfile = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.clipboard = "unnamedplus"
 vim.o.exrc = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.swapfile = false
-
-vim.opt.clipboard = "unnamedplus"
 
 -- LSP settings
 vim.diagnostic.config({
@@ -26,7 +53,12 @@ vim.diagnostic.config({
 })
 
 local servers = { "lua_ls", "pyright", "texlab", "marksman"}
+vim.lsp.enable(servers)
 
+
+--------------------------------------------------
+-- PLUGINS  
+--------------------------------------------------
 vim.pack.add({
 	"https://github.com/nvim-mini/mini.nvim",
 	"https://github.com/neovim/nvim-lspconfig",
@@ -44,6 +76,8 @@ vim.pack.add({
  	"https://github.com/iamcco/markdown-preview.nvim",
     "https://github.com/xiantang/darcula-dark.nvim",
     "https://github.com/3rd/image.nvim",
+	"https://github.com/rmagatti/auto-session", -- TODO: autosession doesn't word for some reason
+    "https://github.com/AnsonH/copy-python-path.nvim",
     { src = 'https://github.com/saghen/blink.cmp', version = vim.version.range('1.x') }
 })
 
@@ -71,6 +105,7 @@ require("mini.pick").setup()
 require("mini.extra").setup()
 require("mini.icons").setup()
 
+-- LSP
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = servers
@@ -81,81 +116,23 @@ require("gitsigns").setup()
 
 require("blink.cmp").setup()
 require("lazydev").setup()
-require("image").setup({
-  backend = "kitty", -- or "ueberzug" or "sixel"
-  processor = "magick_cli", -- or "magick_rock"
-  integrations = {
-    markdown = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      only_render_image_at_cursor_mode = "popup", -- or "inline"
-      floating_windows = false, -- if true, images will be rendered in floating markdown windows
-      filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-    },
-    asciidoc = {
-      enabled = true,
-      clear_in_insert_mode = false,
-      download_remote_images = true,
-      only_render_image_at_cursor = false,
-      only_render_image_at_cursor_mode = "popup",
-      floating_windows = false,
-      filetypes = { "asciidoc", "adoc" },
-    },
-    neorg = {
-      enabled = true,
-      filetypes = { "norg" },
-    },
-    rst = {
-      enabled = true,
-    },
-    typst = {
-      enabled = true,
-      filetypes = { "typst" },
-    },
-    html = {
-      enabled = false,
-    },
-    css = {
-      enabled = false,
-    },
-  },
-  max_width = nil,
-  max_height = nil,
-  max_width_window_percentage = nil,
-  max_height_window_percentage = 50,
-  scale_factor = 1.0,
-  kitty_direct_chunk_size = 4096, -- chunk size for direct Kitty graphics protocol transmission
-  window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-  window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
-  editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
-  tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-  hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+require("auto-session").setup( {
+    cwd_change_handling = true,
+    opts = {
+        bypass_save_filetypes = { "oil" },
+    }
+} )
+require("image").setup()
+
+-- Theme
+require("darcula").setup({
+	theme = "darcula"  -- loads built-in colors/themes/darcula.json
 })
--- Enable syntax highlight
-vim.api.nvim_create_autocmd('FileType', {
-	callback = function() pcall(vim.treesitter.start) end
-})
-vim.lsp.enable(servers)
 
--- Keymap
-local function get_current_path()
-	if vim.bo.filetype == "oil" then
-		return require("oil").get_current_dir()
-	end
-	return vim.api.nvim_buf_get_name(0)
-end
 
-local function get_git_root()
-	local path = get_current_path()
-    if path == nil then
-        return
-    else
-	    return vim.fs.root(path, ".git")
-    end
-end
-
+--------------------------------------------------
+-- KEYMAPS
+--------------------------------------------------
 vim.keymap.set("n", "<leader>sh", ":Pick help<CR>")
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
@@ -183,32 +160,15 @@ vim.keymap.set("n", "[c", function() require("gitsigns").nav_hunk("last") end)
 vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<CR>")
 vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<CR>")
 
-vim.keymap.set("n", "<leader>h", vim.diagnostic.open_float)
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>")
+-- Exit terminal-mode (go back to Normal mode) using Esc
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
 
-
--- Theme
-require("darcula").setup({
-	theme = "darcula"  -- loads built-in colors/themes/darcula.json
-})
-
--- Load .nvim.lua from project folder
-vim.opt.exrc = true
-vim.keymap.set("n", "]c", function() require("gitsigns").nav_hunk("next") end)
-vim.keymap.set("n", "[c", function() require("gitsigns").nav_hunk("last") end)
-
--- Tasks
-vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<CR>")
-vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle<CR>")
+-- Features
+vim.keymap.set("n", "<leader>yf", function()
+	vim.fn.setreg("+", vim.fn.expand("%"))
+	vim.notify("Copied: " .. vim.fn.expand("%"))
+end)
+vim.keymap.set("n", "<leader>yp", ':CopyPythonPath dotted<CR>')
 
 vim.keymap.set("n", "<leader>h", vim.diagnostic.open_float)
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<cmd>w<CR>")
 
-
--- Theme
-require("darcula").setup({
-	theme = "darcula"  -- loads built-in colors/themes/darcula.json
-})
-
--- Load .nvim.lua from project folder
-vim.opt.exrc = true
